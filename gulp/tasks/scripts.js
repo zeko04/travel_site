7c51fe4fb5c8f187782a750da160b290829df093
Milos <zecevic.milos@hotmail.com>
2018-03-19 00:32:03 +0100

    Finished the reveal on scroll

--- gulp/tasks/scripts.js
+++ gulp/tasks/scripts.js
@@ -5,7 +5,7 @@ gulp.task('scripts', function(callback){
     webpack(require('../../webpack.config.js'), function(err, stats){
       
         if(err){
-            console.log(err.toString());
+            console.log('\x1b[33m%s\x1b[0m', err.toString());
         }
         console.log('****************STATS*************');
         console.log('\x1b[36m%s\x1b[0m', stats.toString());
