58f2ceaa48be236eab1228b19c31b63ad55e50fc
Milos <zecevic.milos@hotmail.com>
2018-03-19 02:32:09 +0100

    Finishead Sticky header

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,8 +1,11 @@
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
+import StickyHeader from './modules/StickyHeader';
 import $ from 'jquery';
 
 
 var mobileMenu = new MobileMenu();
 new RevealOnScroll($('.feature-item'), "85%");
 new RevealOnScroll($('.testemonial'), '65%');
+
+var stickyHeader = new StickyHeader();
